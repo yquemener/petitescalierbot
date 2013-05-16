411f051469213c799858e44e4e29e969a0a933c8
unknown <Yves@Yves-Desktop-HP.(none)>
2013-05-16 22:26:33 +0900

    Very rudimentary first version : gives only title of top posts

---
+++ main.py
@@ -0,0 +1,46 @@
+# This file is part of PetitEscalier Bot.
+# 
+# PetitEscalier Bot is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as published by
+# the Free Software Foundation.
+# 
+# PetitEscalier Bot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with PetitEscalier Bot.  If not, see <http://www.gnu.org/licenses/>.
+#
+# (C) Copyright Yves Quemener, 2013
+
+import praw
+
+import traceback
+import sys
+import getpass
+
+from time import sleep
+
+reddit = praw.Reddit(user_agent='PetitEscalier bot, by u/keepthepace')
+user='keepthepace'
+print "Password for",user,"?"
+passwd=getpass.getpass()
+reddit.login(user, passwd)
+
+sr = reddit.get_subreddit("petitescalier").get_hot(limit=20)
+
+s=""
+s+='<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />\n'
+for post in sr:
+    s += '<div class="post">'
+    s += '<span class="score">' + str(post.score) + '</span>'
+    s += '<span class="post_title">' + post.title.encode('utf8') + '</span>'
+    s += ' par '
+    s += '<span class="author">' + post.author.name.encode('utf8') + '</span>'
+    s += '</div>' # /post
+    s += '\n'
+
+fi = open("out.html", "w")
+fi.write(s)
+fi.close()
