f4058b7337a1ee322b496b0f876d53ae687c4a3a
Yves Quemener <Iv@laptop>
2013-05-18 01:35:30 +0900

    Now takes all replies and has a (ugly) CSS

--- main.py
+++ main.py
@@ -20,35 +20,71 @@ import traceback
 import sys
 import getpass
 
-from time import sleep
+from time import sleep, asctime
 
-reddit = praw.Reddit(user_agent='PetitEscalier bot, by u/keepthepace')
-user='keepthepace'
-print "Password for",user,"?"
-passwd=getpass.getpass()
-reddit.login(user, passwd)
+def debug():
+    reddit = praw.Reddit(user_agent='PetitEscalier bot, by u/keepthepace')
+    user='keepthepace'
+    print "Password for",user,"?"
+    passwd=getpass.getpass()
+    reddit.login(user, passwd)
+    arr = []
+    for a in reddit.get_subreddit("petitescalier").get_hot(limit=20):
+        arr.append(a)
+    return arr
 
-while True:
-    try:
-        sr = reddit.get_subreddit("petitescalier").get_hot(limit=20)
-
+def add_replies(replies):
+    if len(replies)==0:
+        return ""
+    else:
         s=""
-        s+='<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />\n'
-        for post in sr:
-            s += '<div class="post">'
-            s += '<span class="score">' + str(post.score) + '</span>'
-            s += '<span class="post_title">' + post.title.encode('utf8') + '</span>'
-            s += ' par '
-            s += '<span class="author">' + post.author.name.encode('utf8') + '</span>'
-            s += '</div>' # /post
-            s += '\n'
+        for rep in replies:
+            s+=' <div class="reply">'
+            s+='  <div class="reply_header">'
+            s+='   <span class="score">' + str(rep.score) + '</span>'
+            s+='   <span class="author">' + rep.author.name.encode('utf8') + '</span>'
+            s+='  </div>' # /reply_header
+            s+='  <div class="reply_body">' 
+            s+='<code>' # En attendant un renderer a la reddit
+            s+= rep.body.encode('utf8') + '</code></div>'
+            s+= add_replies(rep.replies)
+            s+=' </div>' # /reply
+    return s
 
-        fi = open("/var/www/Projects/PetitEscalierReadOnly/index.html", "w")
-        fi.write(s)
-        fi.close()
-        sleep(3600) # One update every hour is quite enough for now
-    except:
-        print asctime(),"Exception in main program : "
-        traceback.print_exc()
-        sleep(300) # retry in 5 minutes
+if __name__ == "__main__":
+    reddit = praw.Reddit(user_agent='PetitEscalier bot, by u/keepthepace')
+    user='keepthepace'
+    print "Password for",user,"?"
+    passwd=getpass.getpass()
+    reddit.login(user, passwd)
+
+    while True:
+        try:
+            sr = reddit.get_subreddit("petitescalier").get_hot(limit=20)
+
+            s=""
+            s+='<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />\n'
+            s+= '<link href="css.css" type="text/css" rel="stylesheet"></link>'
+            for post in sr:
+                s += '<div class="post">'
+                s += ' <div class="post_header">'
+                s += '  <span class="score">' + str(post.score) + '</span>'
+                s += '  <span class="post_title">' + post.title.encode('utf8') + '</span>'
+                s += ' par '
+                s += '  <span class="author">' + post.author.name.encode('utf8') + '</span>'
+                s += ' </div>' # /post_header
+                s += ' <div class="post_body">' + post.selftext.encode('utf8') + '</div>'
+                s += add_replies(post.comments)
+                s += '</div>' # /post
+                s += '\n'
+
+            fi = open("/var/www/Projects/PetitEscalierReadOnly/index.html", "w")
+            fi.write(s)
+            fi.close()
+            print "Updated : " + asctime()
+            sleep(3600) # One update every hour is quite enough for now
+        except:
+            print asctime(),"Exception in main program : "
+            traceback.print_exc()
+            sleep(300) # retry in 5 minutes
 
